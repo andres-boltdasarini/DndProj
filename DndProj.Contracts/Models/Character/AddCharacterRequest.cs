45147b219b2ab4ddd2e582a874fa98b1d8f7b97f
Андрей <lau47@mail.ru>
2025-07-01 22:55:44 +0300

    AddCharacterRequest2

--- DndProj.Contracts/Models/Character/AddCharacterRequest.cs
+++ DndProj.Contracts/Models/Character/AddCharacterRequest.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 
 namespace DndProj.Contracts.Models.Character
 {
     public class AddCharacterRequest
     {
         public string? Name { get; set; }
+        public string? Worldview { get; set; }
+        public string? Gender { get; set; }
+        public string? Background { get; set; }
 
-        public int? Strength { get; set; }
+        public int? Move { get; set; }
+        public int? Helth { get; set; }
+        public int? ClassArmor { get; set; }
+        public int? Initiative { get; set; }
+        public Profession? Profession { get; set; }
+        public Race? Race { get; set; }
+        public Skills? Skills { get; set; }
+        public Characteristics? Characteristics { get; set; }
 
-        public int? Dexterity { get; set; }
 
-        public int? Vitality { get; set; }
+        public AddCharacterRequest()
+        {
+            Profession = new Profession();
+            Race = new Race();
+            Skills = new Skills();
+            Characteristics = new Characteristics();
+        }
 
-        public int? Intelligence { get; set; }
-
-        public int? Wisdom { get; set; }
-
-        public int? Charisma { get; set; }
     }
 }
